887b298a52c32fd0413c20ed2a9956f06b7382ea
Huy Long <huylong@Huys-iMac.local>
2021-12-18 11:08:06 +0700

    sever

--- sever/index.js
+++ sever/index.js
@@ -33,7 +33,7 @@ app.use('/api/auth' ,authRouter)
 app.use('/api/post' ,postRouter)
 app.use('/api/product',productRouter)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , () => console.log(`Sever started on PORT ${PORT}`));
  
