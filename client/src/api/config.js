e3aff1191ab65d527060eb637adfb57b46ccd4cc
Huy Long <huylong@Huys-iMac.local>
2021-12-18 10:24:31 +0700

    fix

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 import Cookie from "js-cookie";
 
-const URL = process.env.REACT_APP_ENDPOINT;
+const URL = 'http://localhost:5000'
 
 const createAxios = () => {
   let axiosInstant = axios.create();
